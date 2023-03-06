2b96fc29d506dc192272b1c7c9c578c407b21b19
Mateusz G <garbacik.mateusz@gmail.com>
2023-03-06 22:51:14 +0100

    node js implementation

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,27 @@
 const open = require("open");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+
+const app = express();
 
 (async () => {
   await open("../build/index.html", { wait: true });
 })();
+
+
+app.listen(3001);
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+app.post('/', (req, res) => {
+  res.send('Hello World!');
+  const {data} = req.body;
+  console.log(req.body);
+});
+
+function writeFile(where, what) {
+  fs.writeFile(where, what, { flag: 'w' }, err => {});
+};
+
